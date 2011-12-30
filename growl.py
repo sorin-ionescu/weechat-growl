48b0156e96c085c46cec153787f9fe19491f7f82
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-12-30 15:29:58 -0500

    Bumped version to v1.0.5.

--- growl.py
+++ growl.py
@@ -24,12 +24,13 @@
 
 SCRIPT_NAME = 'growl'
 SCRIPT_AUTHOR = 'Sorin Ionescu <sorin.ionescu@gmail.com>'
-SCRIPT_VERSION = '1.0.4'
+SCRIPT_VERSION = '1.0.5'
 SCRIPT_LICENSE = 'MIT'
 SCRIPT_DESC = 'Sends Growl notifications upon events.'
 
 
 # Changelog
+# 2011-12-30: v1.0.5 Fixed a NonType error.
 # 2011-10-11: v1.0.4 Handle import errors better.
 # 2011-10-10: v1.0.3 Handle Growl exceptions.
 # 2011-10-04: v1.0.2 Growl 1.3 requires GNTP.
